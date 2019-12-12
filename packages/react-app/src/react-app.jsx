1208195f8ad2f66d442e2cbf8b91e7f8f6321cbc
xudaosong <xudaosong@leedarson.com>
2019-12-12 11:36:19 +0800

    init

--- packages/react-app/src/react-app.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-export default function reactApp() {
-  return ReactDOM.render(
-    <h1>Hello, world React App!</h1>,
-    document.getElementById("root")
-  );
-}
