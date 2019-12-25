18a2a35ba7caeedc9195e5e03d630af449d4eee6
xudaosong <xudaosong@leedarson.com>
2019-12-25 14:14:47 +0800

    package

--- packages/react-app/src/react-app.js
+++ packages/react-app/src/react-app.js
@@ -1,5 +1,9 @@
-import React from 'react';
+/*
+ * @Author: xudaosong
+ * @Date: 2019-12-25 12:00:51
+ */
+import React from "react";
 
 export default function reactApp() {
-    return <div>react app 1</div>
+  return <div>react app 1</div>;
 }
